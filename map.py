36702ba68cc08851316a32d5c582501a5cd9eedb
Ivan <mr.vahrushev.97@Gmail.com>
2021-02-27 23:36:50 +0300

    add marker on the map

--- map.py
+++ map.py
@@ -2,4 +2,5 @@ import folium
 
 map = folium.Map(location=[55.753215, 37.622504], zoom_start = 8)
 
+folium.Marker(location=[55.753215, 37.622504], popup = "Moscow", icon=folium.Icon(color = "gray")).add_to(map)
 map.save("map.html")
