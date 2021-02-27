61634d0c242b73b7a804d22ae61c673da2242919
Ivan <mr.vahrushev.97@Gmail.com>
2021-02-28 00:03:47 +0300

    remove marker, add popup with coordinates

--- map.py
+++ map.py
@@ -2,5 +2,6 @@ import folium
 
 map = folium.Map(location=[55.753215, 37.622504], zoom_start = 8)
 
-folium.Marker(location=[55.753215, 37.622504], popup = "Moscow", icon=folium.Icon(color = "gray")).add_to(map)
+map.add_child(folium.LatLngPopup())
+
 map.save("map.html")
